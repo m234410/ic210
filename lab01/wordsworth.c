c52a94e736d7ff624c8ae7d2cd11344f65e8df0e
Daniel Murray <m234410@usna.edu>
2020-08-20 11:41:14 -0400

    msg

---
+++ lab01/wordsworth.c
@@ -0,0 +1,26 @@
+#include "ic210.h"
+
+int main() {
+  fputs("And then my heart with pleasure fills,\n", stdout);
+  fputs("And dances with the daffodils. \n", stdout);
+  fputs(" \n", stdout);
+  fputs("And then my heart with pleasure fills,\n", stdout);
+  fputs("And whistles with the daffodils.\n", stdout);
+  fputs(" \n", stdout);
+  fputs("And then my heart with pleasure fills,\n", stdout);
+  fputs("And sits down with the daffodils. \n", stdout);
+  fputs(" \n", stdout);
+  fputs("And then my heart with pleasure fills, \n", stdout);
+  fputs("And listens to the daffodils. \n", stdout);
+  fputs(" \n", stdout);
+  fputs("I like 1 flower. \n", stdout);
+  fputs("I like 2 flowers. \n", stdout);
+  fputs("I like 3 flowers. \n", stdout);
+  fputs("I like 4 flowers. \n", stdout);
+  fputs("I like 5 flowers. \n", stdout);
+  fputs(" \n", stdout);
+  fputs("And then my heart with pleasure fills, and dances with the daffodils.\n", stdout);
+
+  return 0;
+
+}
