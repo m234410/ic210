f299ac023ba4de6fedfcc309381f3e5853badd3c
Daniel Murray <m234410@usna.edu>
2020-08-25 11:30:45 -0400

    msg

--- hw02/checkout.c
+++ hw02/checkout.c
@@ -1,3 +1,34 @@
-/* Erase this comment and put your name here!
- * Your actual code should start below.
- */
+/* Daniel Murray*/
+
+#include "ic210.h"
+
+int main() {
+
+  fputs("What item are you purchasing? \n", stdout);
+  cstring item; 
+  readstring (item, stdin); 
+  fputs("\n", stdout);
+  
+  fputs("How much does your item cost? \n", stdout);
+  double cost;
+  cost = readnum(stdin); 
+  fputs("\n", stdout);
+
+  fputs("How many items are you purchasing? \n", stdout);
+  double amount; 
+  amount = readnum(stdin);
+  fputs("\n", stdout);
+  
+  double final_cost;
+  final_cost = cost*amount;
+  writenum(amount, stdout);
+  fputs(" ", stdout);
+  fputs(item, stdout);
+  fputs(" ", stdout);
+  fputs("cost $", stdout);
+  writenum(final_cost, stdout);
+  fputs("\n", stdout);
+
+  return 0; 
+}
+
