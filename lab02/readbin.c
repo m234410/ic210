8e7ec4808acf1f0ca2ef93fe06135641660fbae8
Daniel Murray <m234410@usna.edu>
2020-08-27 11:04:18 -0400

    msg

--- lab02/readbin.c
+++ lab02/readbin.c
@@ -1,3 +1,35 @@
-/* Erase this comment and put your name here!
- * Your actual code should start below.
- */
+/* Daniel Murray*/
+
+#include"ic210.h"
+
+int main() {
+fputs("Enter a 4-bit binary number: ", stdout);
+char columnfour = readchar(stdin);
+char columnthree = readchar(stdin);
+char columntwo = readchar(stdin);
+char columnone = readchar(stdin);
+
+fputs("In decimal ", stdout);
+fputc(columnfour, stdout);
+fputc(columnthree, stdout);
+fputc(columntwo, stdout);
+fputc(columnone, stdout);
+
+int four = (int)columnfour;
+four = four - 48;
+int three = (int)columnthree;
+three = three - 48;
+int two = (int)columntwo;
+two = two - 48;
+int one = (int)columnone;
+one = one - 48;
+
+int total = one + (two*2) + (three*4) + (four*8);
+
+fputs(" = ", stdout);
+writenum(total, stdout);
+fputs("\n", stdout);
+
+return 0;
+}
+
