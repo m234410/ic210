2a26e7e49d3f27d7dbf25b488210341cf50309d9
Daniel Murray <m234410@usna.edu>
2020-08-29 14:09:27 -0400

    msg

--- hw04/fields.c
+++ hw04/fields.c
@@ -1,3 +1,24 @@
-/* Erase this comment and put your name here!
- * Your actual code should start below.
- */
+/* Daniel Murray*/
+
+#include"ic210.h"
+#include<math.h>
+
+int main(){
+  fputs("How many football fields? ",stdout);
+  double ff = readnum(stdin);
+ 
+  double ff_feet = ff*300;
+  int miles = ff_feet/5280;
+  int feet = (int)ff_feet%5280;
+  double inches = ((ff_feet - 5280*miles) - feet)*12;
+
+  writenum(miles, stdout);
+  fputs(" miles ", stdout);
+  writenum(feet, stdout);
+  fputs(" feet ", stdout);
+  writenum(inches, stdout);
+  fputs(" inches\n", stdout);
+
+
+return 0;
+}
