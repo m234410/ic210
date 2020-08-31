4c7dda4ad93deb7560eb956dda7c68832b74c228
Daniel Murray <m234410@usna.edu>
2020-08-31 11:36:02 -0400

    msg

--- hw05/capitalize.c
+++ hw05/capitalize.c
@@ -1,3 +1,30 @@
-/* Erase this comment and put your name here!
- * Your actual code should start below.
- */
+/* Daniel Murray */
+
+#include"ic210.h"
+#include<math.h>
+
+int main(){
+  fputs("Input word : ", stdout);
+
+  char letter1 = readchar(stdin);
+  cstring word;
+  readstring(word, stdin);
+
+  int letter1value = (int)letter1;
+  int capletter1value = letter1value-32;
+  char capletter1 = (char)capletter1value;
+
+  fputs("Capitalized: ", stdout);
+
+  if (letter1value<=122&&letter1value>=97){
+    fputc(capletter1, stdout);
+  }
+
+  else 
+    fputc(letter1, stdout);
+
+  fputs(word, stdout);
+  fputs("\n", stdout);
+
+return 0;
+}https://submit.moboard.com/site/receipt.php?submission=3b6dfff3-ad06-4196-9479-81de9d14b26b
--- hw05/sleeping.c
+++ hw05/sleeping.c
@@ -1,3 +1,27 @@
-/* Erase this comment and put your name here!
- * Your actual code should start below.
- */
+/* Daniel Murray*/
+
+#include"ic210.h"
+#include<math.h>
+
+int main(){
+  fputs("Time:",stdout);
+
+  char digit1 = readchar(stdin);
+  char digit2 = readchar(stdin);
+  char digit3 = readchar(stdin);
+  char digit4 = readchar(stdin);
+
+  int hour1 = (int)digit1 - '0';
+  int hour2 = (int)digit2 - '0';
+  int minute1 = (int)digit3 - '0';
+  int minute2 = (int)digit4 - '0';
+
+  int time = 1000*hour1 + 100*hour2 + 10*minute1 + minute2;
+
+  if (time>2300||time<600)
+    fputs(" Asleep\n", stdout);
+  else 
+    fputs(" Awake\n", stdout);
+
+return 0;
+}
