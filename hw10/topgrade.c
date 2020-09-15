8c6a757404287663980a4ee74c4a24143515775c
Daniel Murray <m234410@usna.edu>
2020-09-15 13:37:48 -0400

    msg

--- hw10/topgrade.c
+++ hw10/topgrade.c
@@ -1,3 +1,38 @@
-/* Erase this comment and put your name here!
- * Your actual code should start below.
- */
+/* Daniel Murray*/
+
+#include"ic210.h"
+
+int main(){
+  fputs("Filename: ", stdout);
+  cstring file;
+  readstring(file, stdin);
+  stream grades = fopen(file, "r");
+
+  double topgrade = 0;
+  cstring topname;
+  int n = readnum(grades);
+  cstring word; 
+
+  readstring(word, grades);
+  while(!stringeq(word, "EXAM"))
+    readstring(word, grades);
+  for(int i = 0;i < n; i++){
+    cstring studentName;
+    readstring(studentName, grades);
+    double hwgrade = readnum(grades);
+    double quizgrade = readnum(grades);
+    double examgrade = readnum(grades);
+    double finalgrade = 0.2*hwgrade + 0.2*quizgrade + 0.6*examgrade;
+    if(finalgrade>topgrade){
+      topgrade = finalgrade;
+      strcpy(topname, studentName);
+    }
+  }
+  
+  fputs(topname, stdout);
+  fputs(" has the highest grade of ", stdout);
+  writenum(topgrade, stdout);
+  fputs("\n", stdout);
+
+  return 0;
+}
