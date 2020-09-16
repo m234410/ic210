b400b7cd4c4759930b8fe411160cb442fad17aed
Daniel Murray <m234410@usna.edu>
2020-09-16 08:00:11 -0400

    msg

--- hw11/age.c
+++ hw11/age.c
@@ -1,3 +1,20 @@
-/* Erase this comment and put your name here!
- * Your actual code should start below.
- */
+/* Daniel Murray */
+
+#include"ic210.h"
+
+int main(){
+  int age = 0;
+  int day = 0;
+  int month = 0;
+  int year = 0;
+  cstring name;
+
+  fscanf(stdin," %s %d/%d/%d", name, &month, &day, &year);
+    if(month>9 || (day>14 && month == 9))
+      age = 2020 - year -1;
+    else 
+      age = 2020 - year;
+  fprintf(stdout, "%s is %i years old.\n", name, age);
+
+  return 0;
+}
