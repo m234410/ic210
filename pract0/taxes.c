1a5be05d7d7c82643ffa5b35f47d0b0ec4348259
Daniel Murray <m234410@usna.edu>
2020-09-17 21:20:10 -0400

    msg

--- pract0/taxes.c
+++ pract0/taxes.c
@@ -1,3 +1,41 @@
-/* Erase this comment and put your name here!
- * Your actual code should start below.
- */
+/* Daniel Murray*/
+
+#include<stdio.h>
+#include<string.h>
+#include<stdbool.h>
+typedef char cstring[128];
+
+int main(){
+  cstring filename;
+  double income = 0;
+  scanf("%s $%lg", filename, &income);
+
+  FILE* fin = fopen(filename, "r");
+  if(!fin){
+    printf("File not found!");
+    return 1;
+  }
+
+  double bracket = 0;
+  double lowBound = 0;
+  double hiBound = 0;
+  cstring junk;
+  int check = fscanf(fin, " %lg%c $%lg to $%lg", &bracket, junk, &lowBound, &hiBound);
+  while(check==4){
+    if(income<hiBound && income>lowBound){
+      double tax = bracket*0.01*income;
+      fprintf(stdout, "%g% This is you! You owe $%.1f\n", bracket, tax);
+    }
+    else{
+      fprintf(stdout, "%g% This is not you!\n", bracket);
+    }
+    double currentbracket = bracket;
+    int check = fscanf(fin, " %lg%c $%lg to $%lg", &bracket, junk, &lowBound, &hiBound);
+    if(currentbracket==bracket)
+      return 0;
+  }
+
+
+
+  return 0;
+}
--- pract0/tips.c
+++ pract0/tips.c
@@ -1,3 +1,38 @@
-/* Erase this comment and put your name here!
- * Your actual code should start below.
- */
+/* Daniel Murray*/
+
+#include<stdbool.h>
+#include<stdio.h>
+#include<string.h>
+typedef char cstring[128];
+
+int main(){
+  cstring function;
+  double bill = 0;
+  cstring word2;
+  scanf(" %s $%lg %s", function, &bill, word2);
+  if(!strcmp(function, "calc")){
+    if(!strcmp(word2, "tip")){
+      double tip = 0;
+      scanf(" %lg", &tip);
+      double payAmount = bill*(1+0.01*tip);
+      printf("pay $%.3lf \n", payAmount);
+     }
+     else if(!strcmp(word2, "split")){
+      double ways = 0;
+      double tip = 0;
+      scanf(" %lg ways tip %lg", &ways, &tip);
+      double payAmount = (bill*(1+0.01*tip))/ways;
+      printf("pay $%.3lf \n", payAmount);
+     }
+    }
+    else if(!strcmp(function, "test")){
+      double payAmount = 0;
+      scanf(" $%lg", &payAmount);
+      double tip = payAmount - bill;
+      double tipPercent = 100*(tip/bill);
+      printf("tip %lg", tipPercent);
+      puts("%");
+    }
+  return 0;
+
+  }
