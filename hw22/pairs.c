489cd206f03a94061074090a8c2ab026b3b4dcd1
Daniel Murray <m234410@usna.edu>
2020-10-21 08:31:34 -0400

    msg

--- hw22/pairs.c
+++ hw22/pairs.c
@@ -1,4 +1,36 @@
 /* pairs.c: Read in x and y values and pair them up.
  * Erase this comment and put your name here!
  * Your actual code should start below.
- */
+ Daniel Murray 234410*/
+
+#include<string.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<stdbool.h>
+
+int main(){
+  printf("N: ");
+  int N = 0;
+  scanf(" %d", &N);
+  double xvalues[N];
+  double yvalues[N]; 
+
+  printf("x values: ");
+  for(int i = 0; i < N; ++i){
+    scanf(" %lg", &xvalues[i]);
+  }
+  
+  printf("y values: ");
+  for(int i = 0; i < N; ++i){
+    scanf(" %lg", &yvalues[i]);
+  }
+  
+  for(int i = 0; i < N; ++i){
+    printf("(%g,%g) ", xvalues[i], yvalues[i]);
+  }
+  
+  printf("\n");
+
+  return 0;
+
+}
--- hw22/substring.c
+++ hw22/substring.c
@@ -1,4 +1,41 @@
 /* substring.c: Read two strings and determine whether one contains the other.
  * Erase this comment and put your name here!
  * Your actual code should start below.
- */
+ Daniel Murray 234410*/
+
+#include<string.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<stdbool.h>
+
+bool is_substring(char* test, char* target);
+
+int main(){
+  char target[128] = {0};
+  char test[128] = {0};
+  printf("Target: ");
+  scanf(" %s", target);
+  printf("Test: ");
+  scanf(" %s", test);
+  if(is_substring(test, target))
+    printf("yes\n");
+  else
+    printf("no\n");
+
+  return 0;
+
+}
+
+bool is_substring(char* test, char* target){
+  for(int i = 0; i < strlen(test); ++i){ 
+    if(test[i] == target[0]){
+      for(int j = 1; j < strlen(target); ++j){ 
+        if(!(test[i+j] == target[j]))
+          break;  
+        else if(j == (strlen(target)-1))
+          return true;
+      }
+    }
+  }
+  return false;
+}
